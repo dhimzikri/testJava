7440046789234143d3410ae3c2a2e230db96cf78
dhimzikri <dschok182@gmail.com>
2023-08-10 10:07:05 +0700

    EditPrice+10%new

--- src/TesEdit.java
+++ src/TesEdit.java
@@ -1,39 +1,37 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class TesEdit {
     public static void main(String[] args) {
             try {
-                // Establishing database connection
                 Connection connection = OracleDBConnection.getGlobalConnection();
-
-                // Select the price
-                String selectQuery = "SELECT clean_price FROM DM_TBL_TEST WHERE dates = ? and isin = ?";
-                String date = "2023-07-21"; // Replace with the actual date
-                String isin = "US3138Y4BK43"; // Replace with the actual ISIN ID
+                // Select price
+                String selPrice = "SELECT clean_price FROM DM_TBL_TEST WHERE dates = ? and isin = ?";
+                String date = "2023-08-05"; // date
+                String isin = "US3140L6SU92"; // ISIN ID
+                // set Var Price
                 double price = 0.0;
 
-                try (PreparedStatement selectStatement = connection.prepareStatement(selectQuery)) {
-                    selectStatement.setString(1, date);
-                    selectStatement.setString(2, isin);
-                    try (ResultSet resultSet = selectStatement.executeQuery()) {
+                try (PreparedStatement statPrice = connection.prepareStatement(selPrice)) {
+                    statPrice.setString(1, date);
+                    statPrice.setString(2, isin);
+                    try (ResultSet resultSet = statPrice.executeQuery()) {
                         if (resultSet.next()) {
                             price = resultSet.getDouble("clean_price");
                         } else {
-                            System.out.println("Book not found.");
+                            System.out.println("ISIN or Date not found.");
                             return;
                         }
                     }
                 }
 
-                // Calculate new price
-                double newPrice = price + (0.1 * price);
+                //new_price +10%
+                double newPrice = price + (0.1 * price); // Real price 85.433586
 
                 // Update the price
                 String updateQuery = "UPDATE DM_TBL_TEST SET clean_price = ? WHERE dates = ? and isin = ? ";
@@ -48,8 +46,6 @@ public class TesEdit {
                         System.out.println("Price update failed.");
                     }
                 }
-
-                // Closing the connection
                 connection.close();
             } catch (SQLException e) {
                 e.printStackTrace();
